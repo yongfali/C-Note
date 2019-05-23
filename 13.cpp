8b5d8fecc686e5b52d5f07dac75dbbe02361688a
lyf94 <liyongfa1994@sina.com>
2019-05-23 11:47:22 +0800

    add virtual function examples

--- 13.cpp
+++ 13.cpp
@@ -6,9 +6,39 @@ using namespace std;
  * 第十三章类的继承
  * 
  */
+
+class Base{
+private:
+	int num;
+
+public:
+	virtual void printNum(){
+		cout << "Base class" << endl;
+	}
+
+	virtual ~Base(){
+		cout << "Base delete" << endl;
+	}
+};
+
+class Child : public Base{
+private:
+	int time;
+
+public:
+	void printNum(){
+		cout << "Child class" << endl;
+	}
+	
+	~Child(){
+		cout << "Child delete" << endl;
+	}
+};
+
 int main(){
-
-
-	cout << "test" << endl;
+	Base * bs = new Child;
+	bs->printNum();
+	delete bs;
+	//cout << "test" << endl;
 	return 0;
 }
