e25e58fa1d7eb32dd2d53d0cb25ab71c318736f4
lyf94 <liyongfa1994@sina.com>
2019-06-12 19:54:05 +0800

    add new files

--- STL源码解析/priority_queue_test.cpp
+++ STL源码解析/priority_queue_test.cpp
@@ -7,7 +7,21 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
 	{
-		int 
+		int arr[5] = {0, 3, 6, 2, 8};
+		priority_queue<int> ipq(arr, arr + 5);
+		cout << "size=" << ipq.size() << endl;
+
+		for (int i = 0; i < ipq.size(); ++i)
+		{
+			cout << ipq.top() << " ";
+		}
+		cout << endl;
+
+		while(!ipq.empty()){
+			cout << ipq.top() << " ";
+			ipq.pop();
+		}
+		cout << endl;
 	}
 	return 0;
 }
